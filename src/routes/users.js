b20e57f80333a2473ff82b793ceb3aa2039ef489
dbuarque <diogenes.buarque@gmail.com>
2016-02-29 14:03:51 -0300

    make users route default

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,42 +6,42 @@ let router = express.Router();
 /*
  * GET
  */
-router.get('/users/search', function(req, res) {
+router.get('/search', function(req, res) {
     userController.search(req, res);
 });
 
 /*
  * GET
  */
-router.get('/users', function(req, res) {
+router.get('/', function(req, res) {
     userController.list(req, res);
 });
 
 /*
  * GET
  */
-router.get('/users/:id', function(req, res) {
+router.get('/:id', function(req, res) {
     userController.show(req, res);
 });
 
 /*
  * POST
  */
-router.post('/users', function(req, res) {
+router.post('/', function(req, res) {
     userController.create(req, res);
 });
 
 /*
  * POST
  */
-router.post('/users/:id', function(req, res) {
+router.post('/:id', function(req, res) {
     userController.update(req, res);
 });
 
 /*
  * DELETE
  */
-router.delete('/users/:id', function(req, res) {
+router.delete('/:id', function(req, res) {
     userController.remove(req, res);
 });
 
